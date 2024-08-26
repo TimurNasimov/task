9d12827d642bc7294003d20bf9fbbd37d8d03a58
TimurNasimov <nasimov.t.o@gmail.com>
2024-08-26 22:23:28 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,8 @@
 immutable_var = 123, "string", True
-print(immutable_var)
+print("Immutable tuple:", immutable_var)
  # immutable_var [0] = 324 - Картеж это такая же колекция как и список, но её особенность в том что он не изменямая!
 mutable_list = [1,2,3,"string",5]
 mutable_list[0] = 33
 mutable_list[3] = "main"
 mutable_list[4] = True
-print(mutable_list)
+print("Mutable list:", mutable_list)
