a3b8b0e18b715953d1461463c5d4fbee408b06c3
TimurNasimov <nasimov.t.o@gmail.com>
2024-08-26 03:57:38 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = 'Timur'
+print('Name: ' + name)
+age = 42
+print('Age: ' + str(age))
+age = age + 2
+print('New Age: ' + str(age))
+is_student = True
+print('Is Student: ' + str(is_student))
