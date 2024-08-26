f74c029b2484001bb7e186ee9f670d6330627c30
TimurNasimov <nasimov.t.o@gmail.com>
2024-08-26 03:31:47 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+The_number_of_completed_DZ = 12
+Number_of_hours_spent = 1.5
+Course_name ='Python'
+Time_for_one_task = Number_of_hours_spent / The_number_of_completed_DZ
+print('Курс: ' + Course_name +',','всего задачи:' + str(The_number_of_completed_DZ) + ',', 'затрачено часов:' + str(Number_of_hours_spent) + ',', 'среднее время выполнения ' + str(Time_for_one_task), 'часа.')
