b47ded3924e9b4dcc691d4e3015d5bd6bfcaae3f
TimurNasimov <nasimov.t.o@gmail.com>
2024-10-13 20:24:28 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,5 +1,6 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number = str(str_number.replace('0', ''))
     first = int(str_number[0])
     if len(str_number) <= 1:
         return first
